bbd13ca7e5a1904d36bfea84f875fac89510635d
Daniel Mason <daniel@danielmason.com>
2013-04-30 21:12:44 +0100

    Argh!

---
+++ WanderingStar/src/Entity3D.h
@@ -0,0 +1,23 @@
+
+/**
+ * Main App Class
+ */
+
+#pragma once
+
+#include "Includes.h"
+
+namespace ws {
+
+	class Entity3D {
+
+	protected:
+		Vec3f pos;
+		Vec3f 
+
+	public:
+		Entity3D(Vec3f pos);
+
+	};
+
+};
